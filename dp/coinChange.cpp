f37a6567168b81de012bf6c091321ab643a6111a
Asthasachan <46759177+Asthasachan@users.noreply.github.com>
2021-07-06 16:56:45 +0530

    Create coinChange.cpp

---
+++ dp/coinChange.cpp
@@ -0,0 +1,23 @@
+long long int rod( int S[], int n, int tot ,vector<vector<long long int>>&dp){
+        if(tot==0){
+            return 1;
+        }
+        if(n==0 || tot<0){
+            return 0;
+        }
+        if(dp[n][tot]!=-1){
+            return dp[n][tot];
+        }
+        if(S[n-1]<=tot){
+            return dp[n][tot]= rod(S,n,tot-S[n-1],dp)+rod(S,n-1,tot,dp);
+        }
+        else{
+            return dp[n][tot]=rod(S,n-1,tot,dp);
+        }
+  }
+    long long int count( int S[], int n, int tot )
+    {    vector<vector<long long int>>dp(n+1,vector<long long int>(tot+1,-1));
+        
+       return rod(S,n,tot,dp);
+        //code here.
+    }
