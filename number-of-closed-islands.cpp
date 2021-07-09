66fb9e8c30a621a37b66524a610c061a3e5b57fa
Asthasachan <46759177+Asthasachan@users.noreply.github.com>
2021-07-09 18:24:56 +0530

    Create number-of-closed-islands.cpp

---
+++ number-of-closed-islands.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/number-of-closed-islands/
+ bool dfs(vector<vector<int>>& grid,int i,int j){
+        if(i<0||j<0||i>=grid.size()||j>=grid[0].size()){
+            return false;
+        }
+        if(grid[i][j]==1){
+            return true;
+        }
+        grid[i][j]=1;
+        bool a=dfs(grid,i-1,j);
+        bool b=dfs(grid,i+1,j);
+        bool c=dfs(grid,i,j-1);
+        bool d=dfs(grid,i,j+1);
+        return a && b && c && d;
+    }
+    int closedIsland(vector<vector<int>>& grid) {
+        int c=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==0){
+                    int k=dfs(grid,i,j);
+                    if(k==1){
+                        c=c+1;
+                    }
+                }
+            }
+        }
+        return c;
+    }
