d3920b14ce93fc40e359d8ce664bf6470b8acaf4
Asthasachan <46759177+Asthasachan@users.noreply.github.com>
2021-07-13 11:13:25 +0530

    Create letter-combinations-of-a-phone-number.cpp

---
+++ graph/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/letter-combinations-of-a-phone-number/ 100%
+class Solution {
+public:
+    string d[8]={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    void solve(string digits,int i,int n,vector<string>&s,string c=""){
+        if(i==n){
+            s.push_back(c);
+            return;
+        }
+        solve(digits,i+1,n,s,c+d[digits[i]-'2'][0]);
+        solve(digits,i+1,n,s,c+d[digits[i]-'2'][1]);
+        solve(digits,i+1,n,s,c+d[digits[i]-'2'][2]);
+        if(digits[i]=='7'||digits[i]=='9'){
+        solve(digits,i+1,n,s,c+d[digits[i]-'2'][3]);
+            }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.size()==0){
+            return {};
+        }
+        vector<string>s;
+        int n=digits.size();
+        solve(digits,0,n,s);
+        return s;
+    }
+};
